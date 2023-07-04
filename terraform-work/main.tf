64c46ffc2c8aef3be86eb6bba19f0023109a1392
Ayaz <a.ayaz@fp-dbs.com>
2023-07-04 16:51:44 +0200

    modified file

--- terraform-work/main.tf
+++ terraform-work/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "example" {
-    name    = "rg-test-022"
+    name    = "rg-test-33"
     location = "West Europe"
 }
--- terraform-work/provider.tf
+++ terraform-work/provider.tf
@@ -3,3 +3,13 @@ provider "azurerm" {
  subscription_id = "e3f1ea18-d024-4faa-9083-7c2a138bfc85"
  tenant_id  = "45b06557-8a2f-43f9-bd8b-1881de361481"
   }
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "rg-ayaz-testenv-0623"
+        storage_account_name = "sttstenvayazdata0723"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+        access_key           = "85Fp1QjLE9vJKLroZjVH5TQ11TpLEoHJazVYlrA3YeoOPAxL/M/4UofNTre/iixD+dtbB2Jc1kBE+AStcZpa9w=="
+    }
+}
