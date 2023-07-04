ec2370479c8d89ab2dab78c47e68ef93034030c9
Ayaz <a.ayaz@fp-dbs.com>
2023-07-04 14:08:42 +0200

    modified file

--- terraform-work/main.tf
+++ terraform-work/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "example" {
-    name    = "rg-test-01"
+    name    = "rg-test-022"
     location = "West Europe"
 }
--- terraform-work/provider.tf
+++ terraform-work/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
  features {}
  subscription_id = "e3f1ea18-d024-4faa-9083-7c2a138bfc85"
- 
+ tenant_id  = "45b06557-8a2f-43f9-bd8b-1881de361481"
   }
